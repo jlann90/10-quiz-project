682601fd99a77c54841975fe450f4901c8d4fb4a
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-08-10 15:14:47 -0700

    feat: add constants to shuffle answers, add a different screen for when quiz is completed

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -12,13 +15,28 @@ export default function Quiz() {
     });
   }
 
+  console.log(userAnswers);
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  // spreads and stores the answers into a new array, and sorts them randomly, needs to come after the quizIsComplete otherwise it'll crash due to looking for these variables that would no longer exist when all questions have been run through
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-            <li key={answer} class="answer">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
               </button>
